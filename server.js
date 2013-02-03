0cf3444ae247551febd2b9dec13f6e241f1c4eca
Lloyd Hilaiel <lloyd@hilaiel.com>
2013-02-03 15:56:04 -0700

    working first pass - the minimalist persona implementation with node.js

---
+++ server.js
@@ -0,0 +1,39 @@
+var express = require('express'),
+    request = require('request');
+
+var app = express();
+
+app.use(express.cookieParser())
+   .use(express.bodyParser())
+   .use(express.cookieSession({
+     secret: "meh"
+   }))
+   .use(express.static(__dirname + '/static'));
+
+app.get('/', function(req, res) {
+  res.render('index.ejs', { email: req.session.email || null });
+});
+
+app.get('/logout', function(req, res) {
+  req.session = null;
+  res.redirect('/');
+});
+
+app.post('/auth', function(req, res) {
+  request.post({
+    url: 'https://login.persona.org/verify',
+    json: {
+      assertion: req.body.assertion,
+      audience: "http://127.0.0.1:3000"
+    }
+  }, function(e, r, body) {
+    if(body && body.email) {
+      req.session.email = body.email;
+      res.json({ success: true });
+    } else {
+      res.json({ success: false });
+    }
+  });
+});
+
+app.listen(3000);
